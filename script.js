a1e1d5bace2e170322b2f7ac43aa0284ee191c70
Mohammed3Atef <muhammedatef57@gmail.com>
2024-04-22 06:13:36 +0200

    add main files

---
+++ script.js
@@ -0,0 +1,14 @@
+var password = document.getElementById("passowrd");
+var confirmPassword = document.getElementById("confirm-password");
+
+function validatePassword() {
+  if (password.value != confirmPassword.value) {
+    confirmPassword.setCustomValidity("Password Don't Match");
+  } else {
+    confirmPassword.setCustomValidity("");
+  }
+}
+password.onchange = validatePassword;
+confirmPassword.onkeyup = validatePassword;
+// console.loge(password);
+password.innerHTML = "mohammed"
